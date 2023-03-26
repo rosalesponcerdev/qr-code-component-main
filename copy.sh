73b15f92abdfec5812d7d6b13821f4b7cfb00376
Renzo Rosales <rosalesponcer@gmail.com>
2023-03-26 16:47:06 -0500

    ci: Modificar scripts

--- copy.sh
+++ copy.sh
@@ -1,3 +1,7 @@
+rm -r /etc/nginx/html/qr-code-component-main/*
+
+mkdir img css js
+
 cp -rf ./css/* /etc/nginx/html/qr-code-component-main/css/
 cp -rf ./images/* /etc/nginx/html/qr-code-component-main/images/
 cp -rf ./js/* /etc/nginx/html/qr-code-component-main/js/
