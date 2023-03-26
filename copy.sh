8acabcb08c08c3774c34d33937f058e656bb33f5
Renzo Rosales <rosalesponcer@gmail.com>
2023-03-26 17:08:28 -0500

    ci:actualizar scripts

--- copy.sh
+++ copy.sh
@@ -1,7 +1,7 @@
 rm -r /etc/nginx/html/qr-code-component-main/
 mkdir /etc/nginx/html/qr-code-component-main
 
-mkdir /etc/nginx/html/qr-code-component-main/img /etc/nginx/html/qr-code-component-main/css /etc/nginx/html/qr-code-component-main/js
+mkdir /etc/nginx/html/qr-code-component-main/images /etc/nginx/html/qr-code-component-main/css /etc/nginx/html/qr-code-component-main/js
 
 cp -rf ./css/* /etc/nginx/html/qr-code-component-main/css/
 cp -rf ./images/* /etc/nginx/html/qr-code-component-main/images/
