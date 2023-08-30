68f73f6f7a95a44a2c573630db63e8515b771277
rosalesponcer <44475922+rosalesponcer@users.noreply.github.com>
2023-08-30 11:29:23 -0500

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,1 +1,7 @@
-cp -rf ./css/* ./css/images/* ./css/js/* index.html /etc/nginx/html/qr-code-component-main/
+
+mkdir /var/www/briefcase/qr-code-component-main/
+
+mv ./css/* /etc/nginx/html/qr-code-component-main/
+mv ./images/* /etc/nginx/html/qr-code-component-main/
+mv ./js/* /etc/nginx/html/qr-code-component-main/
+mv index.html /etc/nginx/html/qr-code-component-main/
